ead6389bd353106f9df3696f46a68b2692a732dd
cristiaangz <cristiaangz@gmail.com>
2021-01-19 11:43:23 +0400

    Removed use of display so that it works with barebones ESP32.

--- ESP-WROVER-KIT/src/main.cpp
+++ ESP-WROVER-KIT/src/main.cpp
@@ -3,65 +3,21 @@
 #include <SPI.h>
 #include <FS.h>
 #include <Adafruit_GFX.h>
-#include <WROVER_KIT_LCD.h>
 #include <Adafruit_AS7341.h>
 #include <string>
 
-WROVER_KIT_LCD tft;
 Adafruit_AS7341 as7341;
-
-#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
-
-void displayValues(float counts[]) {
-  tft.fillScreen(WROVER_BLACK);
-  unsigned long start = micros();
-  tft.setCursor(0, 0);
-  tft.setTextColor(WROVER_WHITE);  tft.setTextSize(1);
-  tft.println("Hello World!");
-
-  tft.print("F1 415nm : ");
-  tft.println(counts[0]);
-  tft.print("F2 445nm : ");
-  tft.println(counts[1]);
-  tft.print("F3 480nm : ");
-  tft.println(counts[2]);
-  tft.print("F4 515nm : ");
-  tft.println(counts[3]);
-  tft.print("F5 555nm : ");
-  // again, we skip the duplicates  
-  tft.println(counts[6]);
-  tft.print("F6 590nm : ");
-  tft.println(counts[7]);
-  tft.print("F7 630nm : ");
-  tft.println(counts[8]);
-  tft.print("F8 680nm : ");
-  tft.println(counts[9]);
-  tft.print("Clear    : ");
-  tft.println(counts[10]);
-  tft.print("NIR      : ");
-  tft.println(counts[11]);
-}
 
 ///////////////////   SETUP AND LOOP ///////////////
 
 
 void setup() {
   Serial.begin(115200);
-
-  tft.begin();
-  tft.setRotation(1);
-
-  tft.fillScreen(WROVER_BLACK);
   unsigned long start = micros();
-  tft.setCursor(0, 0);
-  tft.setTextColor(WROVER_WHITE);  tft.setTextSize(1);
-  tft.println("Waiting for connection!");
 
   while (!Serial) {
     delay(1);
   }
-
-  tft.println("Serial is Connected");
   
   if (!as7341.begin()){
     Serial.println("Could not find AS7341");
@@ -73,7 +29,6 @@ void setup() {
   as7341.setGain(AS7341_GAIN_256X);
 
   Serial.println("Done with setup");
-  tft.println("Done with setup");
 }
 
 
@@ -82,11 +37,7 @@ void loop(void) {
   uint16_t readings[12];
   float counts[12];
 
-  tft.fillScreen(WROVER_BLACK);
   unsigned long start = micros();
-  tft.setCursor(0, 0);
-  tft.setTextColor(WROVER_WHITE);  tft.setTextSize(1);
-  tft.println("Hello Death!");
 
   if (!as7341.readAllChannels(readings)){
     Serial.println("Error reading all channels!");
@@ -123,7 +74,6 @@ void loop(void) {
   Serial.println(counts[11]);
 
   Serial.println();
-  displayValues(counts);
   
   delay(500);
 }
